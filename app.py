a0a86622dfd3ed1e4b33231dc27394b2cdfd918f
Jordan_Walls-Wilson <jmww1998@gmail.com>
2023-02-10 22:36:39 -0800

    second uplpad

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
 """Blogly application."""
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, redirect, render_template,Request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "oh-so-secret"
 
-debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
 
 
 @app.route("/")
-def user():
+def root():
 
-    users = User.query.all()
-    return render_template('base.html', users=users)
+    
+    return redirect("/users")
 
 @app.route("/users")
-def users():
-    users = db.session.execute(db.select(User).order_by(user.fist_name)).scalars()
-    return render_template("base.html", users=users)
+def users_index():
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template("index.html", users=users)
 
-@app.route("/users/new")
+@app.route("/users/new", methods=["GET"])
 def new_users():
     return redirect("new-user.html")
 
-@app.route("/users/new", methods="POST")
+@app.route("/users/new", methods=["POST"])
 def adding_new_user():
-    first_name = request.form["first_name"]
-    last_name = request.form["last_name"]
-    image = request.form["image"]
+    new_user = User(
+        first_name = Request.form["first_name"],
+        last_name = Request.form["last_name"],
+        image = Request.form["image"] or None)
 
-    new_user = User(first_name=first_name, last_name
-    =last_name, image=image)
+   
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect("base.html")
+    return redirect("/users")
--- models.py
+++ models.py
@@ -2,23 +2,27 @@
 from flask_sqlalchemy import SQLAlchemy
 
 
-
-
 db = SQLAlchemy()
 
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
-
-class User(db.model):
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+  
+class User(db.Model):
 
     __tablename__ = "users"
 
     id = db.Column(db.Integer,primary_key=True, autoincrement=True)
     
-    first_name = db.Column(db.String(40), nullable=False)
+    first_name = db.Column(db.Text, nullable=False)
     
-    last_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
 
-    image_url = db.Column(db.Text)
+    image_url = db.Column(db.Text, nullable= False, default = DEFAULT_IMAGE_URL)
 
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+def connect_db(app):
+
+    db.app = app
+    db.init_app(app)
